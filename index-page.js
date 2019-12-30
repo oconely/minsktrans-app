90489723c0b4abbca4a57cb34a58eabff8ce3814
oconely <and.ribaltovsky@gmail.com>
2019-12-30 21:30:23 +0300

    add project bootstrap files

---
+++ index-page.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["index-page"],{
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXgtcGFnZS5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBIiwic291cmNlUm9vdCI6IiJ9
