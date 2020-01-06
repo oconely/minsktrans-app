0b350555845f7a64dacc517cb72cf3aeeabad4fc
oconely <and.ribaltovsky@gmail.com>
2020-01-06 21:36:09 +0300

    styling desktop version of StatusBar and connecting it to store

--- src/components/React/RoutesList/index.js
+++ src/components/React/RoutesList/index.js
@@ -7,23 +7,23 @@ let RoutesList = ({ routes, handleClickByRoute, activeRouteId, pending }) => {
 
     return(
         !pending ? (
-            <ul className="RoutesList">
-                {routes && routes.map(r => {
-                    return <TransportRoute 
-                        key={r.routeid}
-                        routeName={r.routename}
-                        onClick={handleClickByRoute}
-                        routeId={r.routeid}
-                        isActive={activeRouteId === r.routeid}
-                        routeNumber={r.routenum}
-                        weekdays={r.weekdays}
-                        dateStart={r.datestart.replace(/<br\s*[\/]?>/gi, "\r")}
-                        routeDirection={r.routetype}
-                        routeOperator={r.operator}
-                        transportType={r.transport}
-                    />
-                })}
-            </ul>
+                <ul className="RoutesList">
+                    {routes && routes.map(r => {
+                        return <TransportRoute 
+                            key={r.routeid}
+                            routeName={r.routename}
+                            onClick={handleClickByRoute}
+                            routeId={r.routeid}
+                            isActive={activeRouteId === r.routeid}
+                            routeNumber={r.routenum}
+                            weekdays={r.weekdays}
+                            dateStart={r.datestart.replace(/<br\s*[\/]?>/gi, "\r")}
+                            routeDirection={r.routetype}
+                            routeOperator={r.operator}
+                            transportType={r.transport}
+                        />
+                    })}
+                </ul>
         ) : (
             <div>Загружаем маршруты</div>
         )
--- src/components/React/StatusBar/index.js
+++ src/components/React/StatusBar/index.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import Icon from '../SVGIcon';
 import './StatusBar.css';
+import { connect } from 'react-redux';
 
-const StatusBar = ({ typeFilter = ['bus', 'tram', 'trol'], directionFilter }) => (
-    <div className="StatusBar">
-        <div className="StatusBar__row">
-            <h2 className="StatusBar__title">Выбранные маршруты:</h2>
-            <ul className="ul-reset StatusBar__list">
-                {typeFilter.map((type, i) => 
-                    <li className=" StatusBar__item" key={i}>
-                        <Icon className="StatusBar__icon" name={type} />
-                    </li>    
-                )}
-            </ul>
+const StatusBar = ({ typeFilters, directionFilter }) => {
+    typeFilters = !typeFilters.length ? ['bus', 'tram', 'trol'] : typeFilters
+    return (
+        <div className="StatusBar">
+            <div className="StatusBar__row">
+                <h2 className="StatusBar__title">Выбранные маршруты:</h2>
+                <ul className="ul-reset StatusBar__list">
+                    {typeFilters.map((type, i) => 
+                        <li className=" StatusBar__item StatusBar__item_icon" key={i}>
+                            <Icon className="StatusBar__icon" name={type} />
+                        </li>    
+                    )}
+                </ul>
+            </div>
+            <div className="StatusBar__row">
+                <h2 className="StatusBar__title">Направления:</h2>
+                <ul className="ul-reset StatusBar__list">
+                    <li className="StatusBar__direction StatusBar__item" >A>B</li>
+                </ul>
+            </div>
         </div>
-        <div className="StatusBar__row">
-            <h2 className="StatusBar__title">Направления:</h2>
-            <ul className="ul-reset StatusBar__list">
-                <li className="StatusBar__direction StatusBar__item" >A>B</li>
-            </ul>
-        </div>
-    </div>
-);
+    );
+};
 
-export default StatusBar;
+const mapStateToStatusBarProps = state => ({
+    typeFilters: state.filters
+});
+
+export default connect(
+    mapStateToStatusBarProps
+)(StatusBar);
